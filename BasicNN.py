a89966c6d08e93533b336c35476f7bc34b176257
araju8844 <araju8844@gmail.com>
2021-01-12 22:26:38 -0500

    created separate testing and training sets

--- BasicNN.py
+++ BasicNN.py
@@ -26,8 +26,12 @@ unbalancedFiles = [
     "D:/Research PSU 2020/Clean Unbalanced MEMS/171_25Hz_10s.xlsx",
     "D:/Research PSU 2020/Clean Unbalanced MEMS/172_25Hz_10s.xlsx"]
 
+# CHANGE FRAME LENGTH AND TRAIN/TEST RATIO HERE
 # length of each frame for each training example
 frameLength = 60
+trainRatio = .8
+
+
 allFiles = balancedFiles + unbalancedFiles
 # empty dictionary to put the necessary data in with the function
 numpyDict = {}
@@ -36,6 +40,8 @@ m = initNumpyDict(frameLength, numpyDict, allFiles, balancedFiles)
 # initialize a numpy array that has the space to contain all the training examples
 # array has frameLength*2 when flattened
 allExamples = np.zeros((frameLength*2, m))
+allOutcomes = np.zeros((1, m))
+
 # create the training examples at each frame length and put in allExamples
 # create an array that has the values from 0 to m arranged in a random array
 randomizedIndex = sample(list(range(0, m)), m)
@@ -47,58 +53,18 @@ for sheet in allFiles:
     while j + frameLength < numpyDict[sheet][0].shape[0]:
         allExamples[:, randomizedIndex[i]] = numpyDict[sheet][0][j:j +
                                                                  frameLength].reshape(2*frameLength)
+        allOutcomes[0, randomizedIndex[i]] = sheet in balancedFiles
         # increment j by 30 and the examples index by 1
         j += frameLength//2
         i += 1
 
 print(allExamples.shape)
 
-
-m_train = 4
-m_test = len(allFiles) - m_train
-
-
+# initialize m_train and test
+m_train = int(m*trainRatio)
+m_test = m - m_train
 # initializing the training and testing set arrays and allocating their space
-train_set_x_orig = np.zeros((8000, m_train))
-train_set_y_orig = np.zeros((1, m_train))
-test_set_x_orig = np.zeros((8000, m_test))
-test_set_y_orig = np.zeros((1, m_test))
-
-# for loop to create each training example will randomize separately
-"""for i in allFiles:
-    #read the excel file
-    fileDataframe = pd.read_excel(i)
-    #convert to numpy array
-    fileData = fileDataframe.to_numpy()
-    j = 0
-    #while loop to create each training example
-    while j+60 < fileData.shape[1]:
-        
-        #increment by 30
-        j+= 30    
-"""
-"""for i in allFiles:
-    # read the file
-    dataframe = pd.read_excel(i)
-    # convert to numpy
-    temparr = dataframe.to_numpy()
-    # giving all data a common size by making it shape (4000,2)
-    temparr = np.delete(temparr, np.s_[4000:], 0)
-    print(temparr.shape)
-    # flattening by training example
-    arr_flatten = temparr.reshape(1, temparr.shape[0]*temparr.shape[1])
-    # puts it into training or testing set depending on allocated m_train
-    if counter < m_train:
-        # putting this into the necessary column
-        train_set_x_orig[:, counter] = arr_flatten
-        # if i is from the balanced dataset gives 1 else gives a 0
-        train_set_y_orig[counter] = i in balancedFiles
-    else:
-        # putting this into the necessary column
-        train_set_x_orig[:, counter - m_train] = arr_flatten
-        # if i is from the balanced dataset gives 1 else gives a 0
-        train_set_y_orig[counter - m_train] = i in balancedFiles
-    counter += 1
-"""
-# append correct balanced(1) or unbalanced(0) value to y value array
-# you can use dataframe.to_numpy() to turn into arrays
+train_set_x_orig = allExamples[:, 0:m_train]
+train_set_y_orig = allOutcomes[:, 0:m_train]
+test_set_x_orig = allExamples[:, m_train:]
+test_set_y_orig = allOutcomes[:, m_train:]
