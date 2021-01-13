0df569c563a274a5fdbfe046d0952ec8a4f3b313
araju8844 <araju8844@gmail.com>
2021-01-12 20:27:15 -0500

    turned data reader into a function

--- Data Reader.py
+++ Data Reader.py
@@ -1,27 +1,27 @@
 import pandas as pd
-balanced = 1
-bottomBound = 1999
-txt = open("D:/Research PSU 2020/Shaft_vibration_data-Ankur_Verma/Balanced_shaft_MEMS/160_25Hz_10s.txt")
-data = txt.read()
-datalst = data.split("\n")
-topBound = 7999
-del datalst[topBound+1:]
-del datalst[:bottomBound]
-i = 0
-while i < len(datalst):
-    templst = datalst[i].split(",")
-    if balanced == 0:
-        del templst[:2]
-    else:
-        del templst[:2]
-        del templst[1:4]
-    for j in templst:
-        if j != '':
-            j = float(j)
-    datalst[i] = templst
-    i += 1
-df = pd.DataFrame(datalst)
-writer = pd.ExcelWriter(
-    'D:/Research PSU 2020/Clean Balanced MEMS/160_25Hz_10s.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='newdata', index=False)
-writer.save()
+
+
+def createExcelFile(topBound, bottomBound, textLoc, excelLoc, balanced):
+    txt = open(textLoc)
+    data = txt.read()
+    datalst = data.split("\n")
+    del datalst[topBound+1:]
+    del datalst[:bottomBound]
+    i = 0
+    while i < len(datalst):
+        templst = datalst[i].split(",")
+        if balanced == 0:
+            del templst[:2]
+        else:
+            del templst[:2]
+            del templst[1:4]
+        for j in templst:
+            if j != '':
+                j = float(j)
+        datalst[i] = templst
+        i += 1
+    df = pd.DataFrame(datalst)
+    writer = pd.ExcelWriter(
+        excelLoc, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='newdata', index=False)
+    writer.save()
