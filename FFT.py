ba62e2873e0d76399594d71337bf30b34d12523d
araju8844 <araju8844@gmail.com>
2021-05-26 15:50:59 -0400

    Functioning LeNet Data Arrangement may be wrong

--- FFT.py
+++ FFT.py
@@ -2,8 +2,37 @@ import numpy as np
 from matplotlib import pyplot as plt
 from dataSetCreator import *
 from scipy.fft import fft, fftfreq
-x_train, time_train, y_train, x_test, y_test, time_test = fftDataset()
-yf = fft(x_train)
-xf = fftfreq()
-plt.plot(time_train[0], x_train[0])
-plt.show()
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import importTF
+
+x_train, y_train, x_test, y_test, = lenetDataset()
+x_train = x_train.reshape(-1, 32, 32, 1)
+x_test = x_test.reshape(-1, 32, 32, 1)
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+model = tf.keras.Sequential()
+
+model.add(layers.Conv2D(filters=6, kernel_size=(3, 3),
+                        activation='relu', input_shape=(32, 32, 1)))
+model.add(layers.AveragePooling2D())
+
+model.add(layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu'))
+model.add(layers.AveragePooling2D())
+
+model.add(layers.Flatten())
+
+model.add(layers.Dense(units=120, activation='relu'))
+
+model.add(layers.Dense(units=84, activation='relu'))
+
+model.add(layers.Dense(units=1, activation='sigmoid'))
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
+              loss='binary_crossentropy', metrics=['accuracy'])
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+callbacks = []
+model.fit(x_train, y_train, batch_size=50, epochs=32)
+
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print(val_loss, val_acc)
--- dataSetCreator.py
+++ dataSetCreator.py
@@ -28,8 +28,8 @@ unbalancedFiles = [
 
 # CHANGE FRAME LENGTH AND TRAIN/TEST RATIO HERE
 # length of each frame for each training example
-frameLength = 150
-trainRatio = .8
+frameLength = 32*32
+trainRatio = .80
 
 
 allFiles = balancedFiles + unbalancedFiles
@@ -39,6 +39,7 @@ def createDataSet():
     # empty dictionary to put the necessary data in with the function
     numpyDict = {}
     m = initNumpyDict(frameLength, numpyDict, allFiles, balancedFiles)
+    print(numpyDict)
     # TODO: edit m_train and frameLength
     # initialize a numpy array that has the space to contain all the training examples
     # array has frameLength*2 when flattened
@@ -71,6 +72,53 @@ def createDataSet():
     train_set_x_orig = np.transpose(allExamples[:, 0:m_train])
     train_set_y_orig = np.transpose(allOutcomes[:, 0:m_train])
     test_set_x_orig = np.transpose(allExamples[:, m_train:])
+    test_set_y_orig = np.transpose(allOutcomes[:, m_train:])
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig
+
+
+def lenetDataset():
+    # empty dictionary to put the necessary data in with the function
+    numpyDict = {}
+    m = initNumpyDict(frameLength, numpyDict, allFiles, balancedFiles)
+    # TODO: edit m_train and frameLength
+    # initialize a numpy array that has the space to contain all the training examples
+    # array has frameLength*2 when flattened
+    allWaves = np.zeros((32, 32, m))
+    #allTimes = np.zeros((frameLength, m))
+    allOutcomes = np.zeros((1, m))
+    # temp wave so we can do frame overlap sideways
+    prevWave = np.zeros((32, 16))
+    # create the training examples at each frame length and put in allExamples
+    # create an array that has the values from 0 to m arranged in a random array
+    randomizedIndex = sample(list(range(0, m)), m)
+    i = 0
+    # iterate through each sheet
+    for sheet in allFiles:
+        j = 0
+        # creates the dataset and assigns to random position
+        transposeTst = np.transpose(numpyDict[sheet][0])
+        while j + frameLength < numpyDict[sheet][0].shape[0]:
+            # print("enter")
+            #[0] is wave [1] is time
+            allWaves[:, :, randomizedIndex[i]
+                     ] = transposeTst[0][j:j+frameLength].reshape(32, 32)
+            # allTimes[:, randomizedIndex[i]
+            # ] = transposeTst[1][j:j+frameLength]
+            # print("exit")
+            # Balanced with 1 unbalanced with 0
+            allOutcomes[0, randomizedIndex[i]] = sheet in balancedFiles
+            # increment j by 30 and the examples index by 1
+            j += frameLength//2
+            i += 1
+
+    # initialize m_train and test
+    m_train = int(m*trainRatio)
+    m_test = m - m_train
+    # initializing the training and testing set arrays and allocating their space
+    train_set_x_orig = np.transpose(allWaves[:, :, 0:m_train])
+    train_set_y_orig = np.transpose(allOutcomes[:, 0:m_train])
+    test_set_x_orig = np.transpose(allWaves[:, :, m_train:])
     test_set_y_orig = np.transpose(allOutcomes[:, m_train:])
 
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig
